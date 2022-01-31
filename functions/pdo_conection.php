674b3209d5411cd9c40216364f5cfbde1004253e
Mohammadtahernia <taherniamohammad@gmail.com>
2022-01-31 17:59:35 +0330

    create db

--- functions/pdo_conection.php
+++ functions/pdo_conection.php
@@ -1,1 +1,16 @@
+<?php
+
+
+try {
+    
+    $pdo = new PDO ("mysql:host=localhost;dbname=php_project",'root','');
+    echo 'hi';
+}
+
+catch(PDOException $e){
+    echo 'error' . $e->getMessage();
+    exit;
+}
+
+
 
