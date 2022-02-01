c07ac48701c07aef737f4f5bd5eef1627d832609
Mohammadtahernia <taherniamohammad@gmail.com>
2022-02-01 16:39:35 +0330

    update categories
    Create and read categories from the database

--- functions/pdo_conection.php
+++ functions/pdo_conection.php
@@ -2,13 +2,15 @@
 
 <?php
 
-try {
-    
-    $pdo = new PDO ("mysql:host=localhost;dbname=php_project",'root','');
-    echo 'hi';
-}
 
-catch(PDOException $e){
-    echo 'error' . $e->getMessage();
+
+    global $pdo;
+try {
+    $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ);
+    $pdo = new PDO("mysql:host=localhost;dbname=php_project", 'root', '', $options);
+    return $pdo;
+}
+catch (PDOException $e){
+    echo 'error ' . $e->getMessage();
     exit;
 }
